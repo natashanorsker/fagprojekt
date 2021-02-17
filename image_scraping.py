fa009a96e0f3a875f310f2a9ae1a294725d49440
natashanorsker <natashanorsker@gmail.com>
2021-02-17 12:19:28 +0100

    working image scraping (creates a dict with img urls) - we need to figure out how to store

--- image_scraping.py
+++ image_scraping.py
@@ -2,22 +2,25 @@
 
 # imports
 import requests
+import json
 from bs4 import BeautifulSoup
 
 baseurl = "https://uk.pandora.net/en/jewellery/"
+baseurl = "https://uk.pandora.net/en/jewellery/?start=36&amp;sz=36&amp;format=page-element"
 
 headers = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36'
 }
 
-r = requests.get(
-    "https://uk.pandora.net/en/jewellery/charms/")
-soup = BeautifulSoup(r.content, 'lxml')
+catalog = {}
 
-productList = soup.find_all('div', class_='product-tile')
+for page in range(0, 1078, 36):
+    j = requests.get("https://uk.pandora.net/en/jewellery/?start={}&amp;sz=36&amp;format=page-element".format(page))
+    jewellery = BeautifulSoup(j.content, 'lxml')
 
-productLinks = []
+    productList = jewellery.find_all('input', class_='product-details', attrs={"value": True})
 
-for item in productList:
-    for link in item.find_all('a', href=True):
-        print(link)
+    for item in productList:
+        info_dict = json.loads(item['value'])
+        catalog[info_dict["product_id"]] = info_dict
+
