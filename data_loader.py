03645fa22289b4ff7a869fee9acabbbc208308a9
natashanorsker <natashanorsker@gmail.com>
2021-02-18 15:24:15 +0100

    data_loader that doesn't load already loaded data

--- data_loader.py
+++ data_loader.py
@@ -24,24 +24,38 @@ if consent in ['y', 'yes', 'hell ya', 'ja', 'jeps', 'oui', 'tres bien']:
     catalog = json.loads(a_file.read())
     a_file.close()
 
+    # file that stores products already looked up
+    if os.path.isfile('retrieved.txt'):
+        with open('retrieved.txt', 'r') as f:
+            products_done = json.load(f)
+    else:
+        products_done = {}
+        with open("retrieved.txt", "w") as f:
+            json.dump(products_done, f)
+
 
     for product, info in tqdm(catalog.items()):
+        if product not in products_done.keys():
 
-        new_dir = os.path.join(data_dir, product)
-        try:
-            os.chdir(new_dir)
-        except:
-            os.mkdir(new_dir)
-            os.chdir(new_dir)
-
-        img_urls = info['product_image_url']
-
-        for i in range(len(img_urls)):
+            new_dir = os.path.join(data_dir, product)
             try:
-                urllib.request.urlretrieve(img_urls[i], "{}_{}.jpg".format(product, str(i).zfill(2)))
-
+                os.chdir(new_dir)
             except:
-                text_file = open('../Not_found_imgs.txt', "a")
-                text_file.write("Product: {},  URL:  {}".format(product, img_urls[i]))
-                text_file.close()
-                pass
+                os.mkdir(new_dir)
+                os.chdir(new_dir)
+
+            img_urls = info['product_image_url']
+
+            for i in range(len(img_urls)):
+                try:
+                    urllib.request.urlretrieve(img_urls[i], "{}_{}.jpg".format(product, str(i).zfill(2)))
+
+                except:
+                    text_file = open('../Not_found_imgs.txt', "a")
+                    text_file.write("Product: {},  URL:  {}".format(product, img_urls[i]))
+                    text_file.close()
+                    pass
+
+            products_done[product] = True
+            with open("../retrieved.txt", "w") as f:
+                json.dump(products_done, f)
