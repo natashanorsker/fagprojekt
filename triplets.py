ddf6bd5a27e50a434cb728325699d57ff0e3dda4
natashanorsker <natashanorsker@gmail.com>
2021-03-17 17:01:18 +0100

    positive  image paths function

--- triplets.py
+++ triplets.py
@@ -29,11 +29,28 @@ def category_from_id(id):
     return cat
 
 
-def get_positive_image_paths(image_name, product_id, num_pos_images):
+def get_positive_image_paths(image_path, product_id, num_pos_images):
     image_dir = 'data\\'+str(product_id)
+    #get a list of all imagepaths
     image_names = list_pictures(image_dir)
 
-    return image_names
+    #get random image from list of images
+    random_numbers = np.arange(len(image_names))
+    np.random.shuffle(random_numbers)
+
+    if int(num_pos_images)>(len(image_names)-1):
+        num_pos_images = len(image_names)-1
+
+    pos_count = 0
+    positive_images = []
+
+    for random_number in list(random_numbers):
+        if image_names[random_number] != image_path:
+            positive_images.append(image_names[random_number])
+            pos_count += 1
+            if int(pos_count) > (int(num_pos_images) - 1):
+                break
+    return positive_images
 
 
 
